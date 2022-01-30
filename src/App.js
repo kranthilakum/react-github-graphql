d9ef4cc20ea5f39bb7790a846f9af228f9ed48ca
Kranthi Lakum <kranthi.lakum@gmail.com>
2022-01-30 23:15:35 +0530

    feat: Add a search function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import logo from "./logo.svg";
+import { SearchIcon } from "@heroicons/react/solid";
 import "./App.css";
 import Queries from "./Queries";
 import Repository from "./Repository";
@@ -9,6 +10,7 @@ function App() {
   const [loginName, setLoginName] = useState("");
   const [userName, setUserName] = useState("");
   const [repoList, setRepoList] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch(process.env.REACT_APP_BASE_URL, {
@@ -17,7 +19,7 @@ function App() {
         "Content-Type": "application/json",
         Authorization: `bearer ${process.env.REACT_APP_AUTH_TOKEN}`,
       },
-      body: JSON.stringify(Queries),
+      body: JSON.stringify(Queries(searchTerm)),
     })
       .then((response) => response.json())
       .then((data) => {
@@ -28,7 +30,7 @@ function App() {
         setRepoList(searchResults.nodes);
       })
       .catch((error) => console.error(error));
-  }, []);
+  }, [searchTerm]);
 
   return (
     <div className="App">
@@ -45,6 +47,25 @@ function App() {
           </a>
         </h3>
       </header>
+      <div className="flex flex-col items-center justify-center space-y-4 space-x-6 h-20 min-h-full bg-gray-700">
+        <form name="searchForm">
+          <label className="relative block">
+            <span className="sr-only">Search</span>
+            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+              <SearchIcon className="h-5 w-5 text-slate-400" />
+            </span>
+            <input
+              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Search for repositories..."
+              type="text"
+              name="search"
+              value={searchTerm}
+              onInput={(event) => setSearchTerm(event.target.value)}
+            />
+            {/* https://stackoverflow.com/questions/36683770/how-to-get-the-value-of-an-input-field-using-reactjs */}
+          </label>
+        </form>
+      </div>
       <main className="md:container md:mx-auto">
         {repoList && (
           <div className="grid grid-cols-4 gap-4 auto-rows-auto">
--- src/Queries.js
+++ src/Queries.js
@@ -1,34 +1,39 @@
 const userName = process.env.REACT_APP_USER_NAME;
-const Queries = {
-  query: `
-        {
-          viewer {
-            login
-            name
-          }
-          search(type: REPOSITORY, query: "user:${userName} sort:updated-desc", first: 10) {
-            nodes {
-              ...on Repository {
-                id
-                name
-                url
-                description
-                diskUsage
-                licenseInfo {
-                  spdxId
-                }
-                isPrivate
-                languages(first:5){
-                  nodes {
-                    id
-                    name
-                  }
+const Queries = (searchTerm) => {
+  const searchQuery = searchTerm
+    ? `user:${userName} /${searchTerm}/ sort:updated-desc`
+    : `user:${userName} sort:updated-desc`;
+  return {
+    query: `
+      {
+        viewer {
+          login
+          name
+        }
+        search(type: REPOSITORY, query: "${searchQuery}", first: 10) {
+          nodes {
+            ...on Repository {
+              id
+              name
+              url
+              description
+              diskUsage
+              licenseInfo {
+                spdxId
+              }
+              isPrivate
+              languages(first:5){
+                nodes {
+                  id
+                  name
                 }
               }
             }
           }
         }
-      `,
+      }
+    `,
+  };
 };
 
 export default Queries;
